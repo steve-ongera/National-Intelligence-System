6243e82e57a93db0dd23d6e69e211e2f186f5148
steve <gadafisteve001@gmail.com>
2025-06-12 17:54:43 +0300

    made changes

--- national_intelligence_sytem/settings.py
+++ national_intelligence_sytem/settings.py
@@ -104,10 +104,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # For development (optional)
+STATIC_ROOT = BASE_DIR / 'staticfiles'    # For production (collectstatic target)
 
-STATIC_URL = 'static/'
+# Media files (user uploads like images, files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
